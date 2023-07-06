c1dc600b490816744e24b6f00af3e884f36e9804
Gaurav Mori <gkmori444@gmail.com>
2023-07-06 11:48:32 +0530

    Practical - 22 SingleTon Design Pattern

---
+++ SingleTon/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog.Events;
+using Serilog;
+using SingleTon.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+LoggerConfiguration loggerConfiguration = new LoggerConfiguration();
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+Log.Logger = loggerConfiguration.MinimumLevel.Override("Microsoft", LogEventLevel.Information).Enrich.FromLogContext().WriteTo.Console().CreateLogger();
+
+
+
+
+//builder.Services.AddDbContext<EmployeeDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseSerilogRequestLogging();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
